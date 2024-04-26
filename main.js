39351a494d3538d779b08c23f0c7f14cf429f6ae
Toggle Kit <165154534+ToggleKit@users.noreply.github.com>
2024-04-26 19:04:08 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+import {alertActived} from '../alert.js';
+alertActived();
+import { cartActived } from '../cart.js';
+cartActived();
+import {dropDownActived} from '../dropDown.js';
+dropDownActived()
+import {modalActived} from '../modal.js';
+modalActived();
+import {nestMenu} from '../nestMenu.js';
+nestMenu();
+import {pagerActived} from '../pager.js'
+pagerActived();
+import {parentHideActived} from '../parentHide.js';
+parentHideActived();
+import {rangeCountActived} from '../rangeCount.js';
+rangeCountActived();
+import {sliderActived} from '../slider.js';
+sliderActived();
+import {tabActived} from '../tab.js';
+tabActived();
+import {tooltipActived} from '../tooltip.js';
+tooltipActived();
+import {nextSiblingToggle} from '../nextSiblingToggle.js';
+nextSiblingToggle();
+import {preloderActived} from '../preloder.js';
+preloderActived();
+import {scrollToTopActived} from '../scrollToTop.js';
+scrollToTopActived();
